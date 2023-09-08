2747accae7b286ab9062454576081474aef52ae0
Benjamin <nyllary@gmail.com>
2023-09-07 19:50:59 -0700

    Still working

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*
+*Main: Entry point
+*
+*Return: Function return zero.
+*
+*C: function
+*/
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0);
+    }
