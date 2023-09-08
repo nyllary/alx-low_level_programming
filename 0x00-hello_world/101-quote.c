d7438d31c3183a7c3f7f8bd5a60a68d10e9970d1
Benjamin <nyllary@gmail.com>
2023-09-07 23:09:22 -0700

    anyways

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/*
+ *Main: entery point
+ *
+ *description: 'advance'
+ *
+ *return: always 1
+ */
+
+int main(void)
+{
+  Write(2,
+	"and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+  return (1);
+} 
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+
 /**
- *main - Entry point
- *
- *Return:Always 0(Success)
- */
+*
+*main - Entry point
+*
+*Description:'the puts statement'
+*
+*Return:Always 0(Success)
+*/
 
 int main(void)
 {
