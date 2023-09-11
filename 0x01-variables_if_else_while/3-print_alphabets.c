893a777a312bb34cc58eb1cf43a6b16cadbd62cf
Benjamin <nyllary@gmail.com>
2023-09-11 11:46:39 -0700

    reversed

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int a;
 
-	for (a = 'A'; a <= 'Z'; a++)
-		putchar(a);
 	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
 		putchar(a);
 
 	putchar('\n');
