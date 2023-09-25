032098b874122a2bfc5851cf505e9dfb351025c1
Benjamin <nyllary@gmail.com>
2023-09-25 10:30:44 -0700

    requirement error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _memset - prints constant byte to a pointer
  *
@@ -18,7 +19,39 @@ char *_memset(char *s, char b, unsigned int n)
 
 	dest_s = (char *) s;
 
-	for (byt = 0; byt < n; byt++)
+	for (byt = 0; byt <= n; byt++)
 		dest_s[byt] = b;
 	return (dest_s);
+}
+
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);
+    return (0);
 }
