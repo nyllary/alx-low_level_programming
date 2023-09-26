98126d91f8dbb47f9affb4b2f1d36f5e471f3507
Benjamin <nyllary@gmail.com>
2023-09-25 21:13:29 -0700

    The advance task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - set value of a pointer to a char
+ *
+ * @s: pointer pointed by
+ * @to: pointer pointed too
+ *
+ * Return: Always 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: pointer to receive character
+ * @c: charact in a string
+ *
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int 
