c2dff3c6512dd7ec28c925251a232bbe1d5b49bc
Benjamin <nyllary@gmail.com>
2023-09-26 06:58:56 -0700

    guess what i did

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard concept
+ *
+ * @a: character array
+ *
+ * Return: Always 0
+ */
+void print_chessboard(char (*a)[8]);
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar("%c", a[a][b]);
+		}
+		_putchar('\n');
+	}
+}
