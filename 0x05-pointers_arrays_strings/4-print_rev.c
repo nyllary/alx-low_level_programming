361d9ceb11dee56e7dea9e2e93519dd5267a656d
Benjamin <nyllary@gmail.com>
2023-10-17 05:47:33 -0700

    task 4, print a string in reverse form

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,28 +9,15 @@
  */
 void print_rev(char *s)
 {
-	    int len = 0, index = 0;
+	int len, index;
 
+	len = 0;
 
-	   for (index = len - 1; index >= 0; index--)
-		   _putchar(s[index]);
+	while (len != '\0')
+		len++;
+
+	for (index = len + 1; index >= 0; index--)
+		_putchar(s[index]);
 
 	   _putchar('\n');
-}
-
-int _strlen(char *)
-{
-	int val, coount;
-
-	val = 0;
-
-	for (val = 0; s[val] != '\0'; val++)
-
-int main(void)
-{
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
 }
