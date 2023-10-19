d1ccf2daf86b644e1ec3440da2a30799c64bd0ea
Benjamin <nyllary@gmail.com>
2023-10-19 16:05:18 -0700

    function all lowcase upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * string_toupper - printd the lowecase to uppercase
  *
@@ -14,18 +13,7 @@ char *string_toupper(char *upp)
 	for (var = 0; upp[var] != '\0'; var++)
 	{
 		if (upp[var] >= 'a' && upp[var] <= 'z')
-			upp[var] = upp[var] - 32
+			upp[var] = upp[var] - 32;
 	}
 	return (upp);
-}
-
-int main(void)
-{
-    char str[] = "Look up!\n";
-    char *ptr;
-
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
 }
