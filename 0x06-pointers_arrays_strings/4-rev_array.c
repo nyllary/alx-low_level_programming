cd44edf06743c9bcb4c385104f89c59f63cbc678
Benjamin <nyllary@gmail.com>
2023-10-19 15:58:29 -0700

    reverse string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * reverse_array - prints the string of array in reverse
  *
@@ -8,58 +7,16 @@
  *
  * Return: Always )
  */
-/*
 void reverse_array(int *a, int n)
 {
-	int b, r;
-	r = (n - 1) / 2;
+	int b, temp;
+	int r = n - 1;
 
-	for (b = 0; b < (n / 2); b++)
+	for (b = 0; b < n / 2; b++)
 	{
-		if (r > b && r != '\0')
-			r--;
+		temp = a[b];
+		a[b] = a[r];
+		a[r] = temp;
+		r--;
 	}
 }
-*/void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
-}
-
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
