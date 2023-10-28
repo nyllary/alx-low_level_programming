b5dd99aaf45bc897d25df5f5bc3de4841e9b69a8
Benjamin <nyllary@gmail.com>
2023-10-28 07:13:19 -0700

    Got my head around startic libraries, yeeei

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 		if (*s == '_')
 			j *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0')
+			num = num * 10 + (*s - '0');
 				break;
 	}
 	while (*s++);
