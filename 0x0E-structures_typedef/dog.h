dc086378b1c1b0c3cb9f833618f8144ef872258f
Benjamin <nyllary@gmail.com>
2023-11-07 22:37:40 -0800

    Betty style checking went wrong

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,33 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure for dogs
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-typedef struct
+
+/**
+ * struct dog_t - allias for struct
+ * @name: element 1
+ * @age: element 2
+ * @owner: 3rd element
+ */
+typedef struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void *new_dog(char *name, float age, char *owner);
