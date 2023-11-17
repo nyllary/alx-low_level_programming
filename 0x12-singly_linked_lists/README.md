9930accca4cc6666cc71bae1322b5528dd4c8afd
Benjamin <nyllary@gmail.com>
2023-11-16 17:47:44 -0800

    restarted everything

--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-Singly linked list project. The concept behind this projrct is data structure. This this project is to help us learning a different way we can declare aparticular type of variables just as the array does but we can actually do more with linked list.
+Singly linked list project. The concept behind this projrct is data structure. This project is to help us learn a different way we can declare a particular type of variable just as the array does but we can actually do more with linked list.
--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,10 +14,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
@@ -26,3 +25,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
