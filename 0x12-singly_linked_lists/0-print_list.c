4a39d73abaf2a9085900d3f754389d3a68f8a992
Benjamin <nyllary@gmail.com>
2023-11-16 16:51:44 -0800

    some king of mistake

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,40 +17,9 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u] %s\n",h->len, h->str);
+		printf("[%u] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
 	}
 	return (count);
-}
-
-int main(void)
-{
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-    return (0);
 }
