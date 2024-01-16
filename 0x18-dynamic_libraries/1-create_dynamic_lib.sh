78b3074bc42f0b3e82b828338fbb90641bc16792
Benjamin <nyllary@gmail.com>
2024-01-15 17:42:45 -0800

    Had some corrections to do in task one

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cmake_minimum_required (VERSION 3.5)
-project (hello_world)
-add_library (func SHARED func.c)
-add_executable (a.out main.c)
+
+# Compile the code into object files
+gcc -c -Wall -Werror -fpic *.c
+
+# Link the object files into a shared library
+gcc -shared -o liball.so *.o
